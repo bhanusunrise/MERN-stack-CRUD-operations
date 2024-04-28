72bfb3bd3ae83e6bf08fda0ddbbaa72a66861aea
bhanusunrise <pasindubhanukagood@gmail.com>
2024-04-28 07:54:23 +0530

    first commit

---
+++ client/src/Users.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Users () {
+    return(
+        <div className = "d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="w- bg-white rounded p-3">
+            
+            </div>
+        </div>
+    )
+}
+
+export default Users;
