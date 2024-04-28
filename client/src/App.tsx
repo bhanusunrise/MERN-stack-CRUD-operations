c7f3066a6233f6ae5e3bdf71cad9c646bb05bf7b
bhanusunrise <pasindubhanukagood@gmail.com>
2024-04-28 09:42:11 +0530

    React Router DOm first Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Users from './Users'
-import CreateUser from './CreateUser'
-import UpdateUser from './UpdateUser'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Users from './Users';
+import CreateUser from './CreateUser';
+import UpdateUser from './UpdateUser';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <BrowserRouter>
-        <Route path='/' element = { <Users />} ></Route>
-        <Route path='/create' element = { <CreateUser />} ></Route>
-        <Route path='/update' element = { <UpdateUser />}></Route>
-      </BrowserRouter>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users />} />
+        <Route path='/create' element={<CreateUser />} />
+        <Route path='/update' element={<UpdateUser />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
