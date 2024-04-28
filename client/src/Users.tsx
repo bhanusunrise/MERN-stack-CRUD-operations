9c7d62cfca9f58e9d62ccab6bb651d356ea1e47a
bhanusunrise <pasindubhanukagood@gmail.com>
2024-04-28 08:14:37 +0530

    Second Commit

--- client/src/Users.tsx
+++ client/src/Users.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 
 function Users () {
+
+    const [users, setUsers] = useState([
+        {Name: "Yousuf", Email: "yousuf@gmail.com", Age: "20"}
+    ])
+
     return(
         <div className = "d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className="w- bg-white rounded p-3">
-            
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            users.map((user) => {
+                                <tr>
+                                    <td>{user.Name}</td>
+                                    <td>{user.Email}</td>
+                                    <td>{user.Age}</td>
+                                    <td><button>Edit</button><button>Delete</button></td>
+                                </tr>
+                            }
+                            )
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     )
